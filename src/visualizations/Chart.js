49d281cc80def0e9ac1fba89273dab05a87fb5ca
Tejas Pancholi <tejaser@users.noreply.github.com>
2018-08-21 07:31:32 +0530

    Unique Key Error in render
    Added key parameter to remove the warning when doing render of rect object in Chart.js

--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -13,12 +13,55 @@ class Chart extends Component {
   static getDerivedStateFromProps(nextProps, prevState) {
     const { data } = nextProps;
     if (!data) return {};
+    const min = d3.min(data, d => d.date);
+    const max = d3.max(data, d => d.date);
+    const xScale = d3
+      .scaleTime()
+      .domain([min, max])
+      .range([0, width]);
 
-    return {};
+    //3. get min and max of the high temprature
+    const [yMin, yMax] = d3.extent(data, d => d.high);
+    const yScale = d3
+      .scaleLinear()
+      .domain([Math.min(yMin, 0), yMax])
+      .range([height, 0]);
+    // console.log(yExtent)
+    // 4. Fill based on avg color
+    const colorExtent = d3.extent(data, d => d.avg).reverse();
+    // console.log(colorExtent)
+    const colorScale = d3
+      .scaleSequential()
+      .domain(colorExtent)
+      .interpolator(d3.interpolateRdYlBu);
+    //5. return array of objects with x, y and height attribute
+    const bars = data.map(d => {
+      return {
+        x: xScale(d.date),
+        y: yScale(d.high),
+        height: yScale(d.low) - yScale(d.high),
+        fill: colorScale(d.avg)
+      };
+    });
+    return { bars };
   }
 
   render() {
-    return <svg width={width} height={height} />;
+    // console.log(this.state.bars);
+    return (
+      <svg width={width} height={height}>
+        {this.state.bars.map((d, i) => (
+          <rect
+            key={i}
+            x={d.x}
+            y={d.y}
+            width={2}
+            height={d.height}
+            fill={d.fill}
+          />
+        ))}
+      </svg>
+    );
   }
 }
 
